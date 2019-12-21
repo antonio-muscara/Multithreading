10d5f9de73adde67b3a7dc4163de98800c1d37d3
Alessio-Vampo-Peano-4A <56753349+Alessio-Vampo-Peano-4A@users.noreply.github.com>
2019-12-21 09:40:14 +0100

    Update Hello.java

--- hihello/Hello.java
+++ hihello/Hello.java
@@ -5,29 +5,14 @@
  */
 package hihello;
 
-import java.lang.Thread;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author vampo.alessio
  */
-public class Hello extends Thread{
-    
-    Hello (){
-        System.out.println("Oggetto Hello istanziato");
-    }
-    
-    public void run(){
+public class Hello extends Thread {
+    public void run() {
         for (int i = 0; i < 10; i++) {
-            
             System.out.println("Hello");
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Hello.class.getName()).log(Level.SEVERE, null, ex);
-            }
         }
     }
 }
