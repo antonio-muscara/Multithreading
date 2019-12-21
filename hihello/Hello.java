244968d34cd9b78065d442049b827e263d7b7070
Alessio-Vampo-Peano-4A <56753349+Alessio-Vampo-Peano-4A@users.noreply.github.com>
2019-12-21 09:48:28 +0100

    Update Hello.java

--- hihello/Hello.java
+++ hihello/Hello.java
@@ -13,6 +13,12 @@ public class Hello extends Thread {
     public void run() {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello");
+            try{
+                Thread.sleep(1000);
+            
+            }catch(Exception E){
+            
+            }
         }
     }
 }
